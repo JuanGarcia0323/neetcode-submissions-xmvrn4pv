8612a781a35b05ab9b8dfc73c1e5702a3906e085
Juan Emilio Garcia <103045493+JuanGarcia0323@users.noreply.github.com>
2026-06-13 12:39:48 -0300

    Add: combination-target-sum-ii - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-4.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} candidates
+     * @param {number} target
+     * @return {number[][]}
+     */
+    combinationSum2(candidates, target) {
+        const nums = candidates.filter(n => n <= target)
+        nums.sort((a, b) => a - b)
+        const response = []
+
+        const sum = (values) => values.length && values.reduce((a,b) => a+b)
+
+        const findCombinations = (i=0, combinations=[]) => {
+            const currentSum = sum(combinations)
+
+            if(currentSum === target){
+                response.push(combinations)
+                return
+            }
+            if(i === nums.length || currentSum > target){
+                return
+            }
+
+            const currentValue = nums[i]
+
+            findCombinations(i+1, [...combinations, currentValue])
+            while(nums[i+1] === currentValue){
+                i++
+            }
+            findCombinations(i+1, [...combinations])
+        }
+
+        findCombinations()
+
+        return response
+    }
+}
